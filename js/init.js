2b73675f36825cfc97f4a04f28e0743d32df9637
BrandonHRocha <87738039+BrandonHRocha@users.noreply.github.com>
2021-07-20 19:26:11 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCkJYqiPnoSaEK3_OAsqv8pr164lFkWcNI",
+    authDomain: "chat801bhr-57617.firebaseapp.com",
+    projectId: "chat801bhr-57617",
+    storageBucket: "chat801bhr-57617.appspot.com",
+    messagingSenderId: "378351091349",
+    appId: "1:378351091349:web:6632ed5218ecc1a3c8e59c",
+    measurementId: "G-7FXW5Z6FVZ"
+    });
